2bd63d705ff5db3892da10f4df768abaffa8c1e2
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2020-10-20 03:38:06 +0200

    Allow PHP 8 and PHPUnit 9 (#11)

--- src/Constraint/IsIdenticalString.php
+++ src/Constraint/IsIdenticalString.php
@@ -15,6 +15,8 @@ if (version_compare(\PHPUnit\Runner\Version::id(), '7.0.0') < 0) {
     class_alias(IsIdenticalStringForV5::class, IsIdenticalString::class);
 } elseif (version_compare(\PHPUnit\Runner\Version::id(), '8.0.0') < 0) {
     class_alias(IsIdenticalStringForV7::class, IsIdenticalString::class);
-} else {
+} elseif (version_compare(\PHPUnit\Runner\Version::id(), '9.0.0') < 0) {
     class_alias(IsIdenticalStringForV8::class, IsIdenticalString::class);
+} else {
+    class_alias(IsIdenticalStringForV9::class, IsIdenticalString::class);
 }
